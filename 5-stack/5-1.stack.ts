df9bc4a6fe1c3cb3639d2e9ef8ef61a134086111
hyejeong <withoo0414@gmail.com>
2022-02-23 22:53:59 +0900

    stack

---
+++ 5-stack/5-1.stack.ts
@@ -0,0 +1,54 @@
+// 배열을 이용하지 않고 하기 array, push 등
+
+// 단일 연결리스트 head -> 1 -> 2 -> 3
+// 이중 연결리스트는 head -> <- 1 -> <- 2 -> <- 3 양방
+
+interface Stack {
+  readonly size: number;
+  push(value: string): void;
+  pop(): string;
+}
+
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+}
+
+class StackImpl implements Stack {
+  private _size: number = 0
+  private head?: StackNode
+
+  constructor(private capacity : number) {
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  push(value: string): void {
+    if(this.capacity === this.size) {
+      throw new Error('Stack is full!!')
+    }
+    const node: StackNode = { value, next: this.head }
+    this.head = node;
+    this._size++;
+  }
+
+  pop(): string { // null == undefined, null !== undefined
+    if(this.head == null) {
+      throw new Error('Stack is empty')
+    }
+    const node = this.head;
+    this.head = node.next
+    this._size--;
+    return node.value
+  }
+}
+
+const stack = new StackImpl(5);
+stack.push('steve 1')
+stack.push('biden 2')
+stack.push('bob 3')
+while(stack.size !== 0) {
+  console.log(stack.pop());
+}
