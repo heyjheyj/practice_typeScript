0cefa48b5c5e42d2cadb38716baafbb254922e99
hyejeong <withoo0414@gmail.com>
2022-05-15 00:02:51 +0900

    8.error-handling

---
+++ 8.exception/8-2.handling.ts
@@ -0,0 +1,35 @@
+class NetworkClient {
+    tryConnect(): void {
+        throw new Error('cannot connect')
+    }
+}
+
+class UserService {
+    constructor(private client: NetworkClient) {
+    }
+
+    login() {
+            this.client.tryConnect()
+        }
+        // login.
+}
+
+class App {
+    constructor(private userService: UserService) {}
+        run() {
+        try {
+            this.userService.login()
+        } catch(error) {
+            // show dialog to user
+        }
+
+        }
+}
+
+const client = new NetworkClient()
+const service = new UserService(client)
+const app = new App(service)
+app.run()
+
+// 에러 처리를 어디서 하는 게 좋을까 고민을 해보자
+// 의미있는 에러 처리를 할 수 있는 곳은? 어딜지?!
