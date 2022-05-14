53f502c8338871783164feb98d7aedebc9ac3100
hyejeong <withoo0414@gmail.com>
2022-05-14 18:58:23 +0900

    stack

--- 5-stack/5-1.stack.ts
+++ 5-stack/5-1.stack.ts
@@ -11,44 +11,44 @@ interface Stack {
 
 type StackNode = {
   readonly value: string;
-  readonly next?: StackNode;
-}
+  readonly next?: StackNode; // StackNode | undefined;
+};
 
 class StackImpl implements Stack {
-  private _size: number = 0
-  private head?: StackNode
+  private _size: number = 0; // 내부에서 쓰이는 변수 앞에 언더바
+  private head?: StackNode; // StackNode | undefined;
 
-  constructor(private capacity : number) {
-  }
+  constructor(private capacity: number) {}
 
   get size() {
     return this._size;
   }
 
   push(value: string): void {
-    if(this.capacity === this.size) {
-      throw new Error('Stack is full!!')
+    if (this.capacity === this.size) {
+      throw new Error("Stack is full!!");
     }
-    const node: StackNode = { value, next: this.head }
+    const node: StackNode = { value, next: this.head };
     this.head = node;
     this._size++;
   }
 
-  pop(): string { // null == undefined, null !== undefined
-    if(this.head == null) {
-      throw new Error('Stack is empty')
+  pop(): string {
+    // null == undefined, null !== undefined
+    if (this.head == null) {
+      throw new Error("Stack is empty");
     }
     const node = this.head;
-    this.head = node.next
+    this.head = node.next;
     this._size--;
-    return node.value
+    return node.value;
   }
 }
 
 const stack = new StackImpl(5);
-stack.push('steve 1')
-stack.push('biden 2')
-stack.push('bob 3')
-while(stack.size !== 0) {
+stack.push("steve 1");
+stack.push("biden 2");
+stack.push("bob 3");
+while (stack.size !== 0) {
   console.log(stack.pop());
 }
