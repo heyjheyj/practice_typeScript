7d3c6da91d6dcb5e652c7b52db68af9d58563e98
hyejeong <withoo0414@gmail.com>
2022-05-17 21:43:56 +0900

    8-3.error handling 연습

--- 8.exception/8-3.errorState.ts
+++ 8.exception/8-3.errorState.ts
@@ -1,49 +1,46 @@
 {
-    type SuccessState = {
-        result: 'success'
+  type SuccessState = {
+    result: "success";
+  };
+
+  type NetworkErrorState = {
+    result: "fail";
+    reason: "offline" | "down" | "timeout" | "logout";
+  };
+
+  type ResultState = SuccessState | NetworkErrorState;
+
+  class NetworkClient {
+    tryConnect(): ResultState {}
+  }
+
+  class UserService {
+    constructor(private client: NetworkClient) {}
+
+    login() {
+      this.client.tryConnect();
     }
 
-    type NetworkErrorState = {
-        result : 'fail';
-        reason: 'offline' | 'down' | 'timeout'
+    // login.
+  }
+
+  class App {
+    constructor(private userService: UserService) {}
+
+    run() {
+      try {
+        this.userService.login();
+      } catch (error) {
+        // show dialog to user
+        // catch로 error를 받으면  error는 any타입이므로 지정
+      }
     }
+  }
 
-    type ResultState = SuccessState | NetworkErrorState
-
-    class NetworkClient {
-        tryConnect(): ResultState {}
-    }
-
-    class UserService {
-        constructor(private client: NetworkClient) {
-        }
-
-        login() {
-            this.client.tryConnect()
-        }
-
-        // login.
-    }
-
-    class App {
-        constructor(private userService: UserService) {
-        }
-
-        run() {
-            try {
-                this.userService.login()
-            } catch (error) {
-                // show dialog to user
-                // catch로 error를 받으면  error는 any타입이므로 지정
-            }
-
-        }
-    }
-
-    const client = new NetworkClient()
-    const service = new UserService(client)
-    const app = new App(service)
-    app.run()
+  const client = new NetworkClient();
+  const service = new UserService(client);
+  const app = new App(service);
+  app.run();
 }
 
 // 에러 처리를 어디서 하는 게 좋을까 고민을 해보자
