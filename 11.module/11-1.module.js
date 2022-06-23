9b8da54ee705cc0092e7dfd83fb433d2d6368244
hyejeong <withoo0414@gmail.com>
2022-06-23 12:51:31 +0900

    11.module

---
+++ 11.module/11-1.module.js
@@ -0,0 +1,7 @@
+export function add(a, b) {
+    return a + b
+}
+
+export function print(a, b) {
+    return console.log('print:', a, b)
+}
---
+++ 11.module/11-2.module.js
@@ -0,0 +1,4 @@
+import {add, print} from './11-1.module.js'
+
+console.log(add(1,2))
+console.log(print('hello', 'world'))
